46376329492c01b176cf1ffe49878dd9cb45f094
Thomas Ragot <thomasragot@gmail.com>
2017-12-01 17:06:31 -0500

    first commit

---
+++ konami/src/components/konami.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Konami extends Component {
+    render() {
+        return (
+             <div>Hello this is konami</div>
+        );
+    };
+}
+
+export default Konami;
